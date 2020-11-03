09fc4962d99682e06cb47205e38bf09c2678a232
Kazuki Matsuzaki <kazuki0924@gmail.com>
2020-11-03 11:45:42 +0900

    1

---
+++ types/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println(-200, -100, 0, 50, 100,)
+
+	fmt.Println(
+		-50.5, -20.5, -0., 1., 100.56,
+	)
+
+	fmt.Println(
+		true, false,
+	)
+
+	fmt.Println(
+		"",
+		"hi",
+		"誉",
+	)
+
+	var myAge int
+	var yourAge int
+	var temperature float64
+
+	fmt.Println(myAge, yourAge)
+	fmt.Println(temperature)
+}
